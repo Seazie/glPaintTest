532286a744176170ac2a25a5513d731b8cddc8bc
Seazie <carlitoszaza@gmail.com>
2025-06-15 11:07:44 -0500

    edition for shader header file

--- shader.h
+++ shader.h
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+using namespace std;
 
 class Shader
 {
@@ -15,13 +16,77 @@ class Shader
         unsigned int ID;
     
     // this constructor will read and build the shader
-    Shader(const char* vertexPath, const char* fragmentPath);
+    Shader(const char* vertexPath, const char* fragmentPath)
+    {
+        // retrieve vertex/fragment source code from filePath
+        string vertexCode;
+        string fragmentCode;
+        ifstream vShaderFile;
+        ifstream fShaderFile;
+        // ensure ifstream objects can throw exceptions
+        vShaderFile.exceptions (ifstream::failbit | ifstream::badbit);
+         fShaderFile.exceptions (ifstream::failbit | ifstream::badbit);
+         try
+         {
+            // open files
+            vShaderFile.open(vertexPath);
+            fShaderFile.open(fragmentPath);
+            stringstream vShaderStream, fShaderStream;
+            // readn file's buffer contents into streams
+            vShaderStream << vShaderFile.rdbuf();
+            fShaderStream << fShaderFile.rdbuf();
+            // close file handlers
+            vShaderFile.close();
+            fShaderFile.close();
+            // convert stream into string
+            vertexCode = vShaderStream.str();
+            fragmentCode = fShaderStream.str();
+
+         }
+         catch (ifstream::failure& e)
+         {
+            cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ: " << e.what() << endl;
+         }
+         const char* vShaderCode = vertexCode.c_str();
+         const char* fShaderCode = fragmentCode.c_str();
+         // 2. compile shaders
+         unsigned int vertex, fragment;
+          
+         vertex = glCreateShader(GL_VERTEX_SHADER);
+         glShaderSource(vertex, 1, &vShaderCode, NULL);
+         glCompileShader(vertex);
+         checkCompileErrors(vertex, "VERTEX");
+    }
     // use/activate the shader
     void use();
     // utility uniform function
     void setBool(const std::string &name, bool value) const;
     void setInt(const std::string &name, int value) const;
     void setFloat(const std::string &name, float value) const;
+    
+};
+private:
+    void checkCompileErrors(unsigned int shader, string type)
+{
+    int success;
+    char infoLog[1024];
+    if (type != "PROGRAM")
+    {
+        glGetShaderiv(shader, GL_COMPILE_STATUS,  &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+            cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << endl;
+        }
+    }
+    else{
+        glGetProgramiv(shader, 1024, NULL, infoLog);
+        if (!success)
+        {
+            glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+            cout << "error::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << endl;
+        }
+    }
 };
 
 #endif
